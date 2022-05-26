41b93ff5f945b8fd9c5777914889c8c801051c7b
cteyr123 <cteyr123@gmail.com>
2022-05-26 14:32:38 -0400

    botton container add

---
+++ src/components/Input.tsx
@@ -0,0 +1,9 @@
+const Input = () => {
+  return (
+    <div className="div-input-button">
+      <input type="text" placeholder="Shorten a link here..." />
+      <button type="button">Shorten It!</button>
+    </div>
+  );
+};
+export { Input };
